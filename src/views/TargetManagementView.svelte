97b9bef4674e7bb3b6aead64361d7ed823dde9a2
MrMelon54 <github@mrmelon54.com>
2023-11-13 13:26:45 +0000

    Convert target tables to one generic component

--- src/views/TargetManagementView.svelte
+++ src/views/TargetManagementView.svelte
@@ -1,31 +1,26 @@
-<script lang="ts">
-<<<<<<< TargetManagement.svelte
-  import RedirectCreator from "../components/RedirectCreator.svelte";
-  import RedirectRow from "../components/RedirectRow.svelte";
-  import {getBearer} from "../stores/login";
-  import type {CSPair} from "../types/cspair";
-  import {type Redirect, redirectEqual} from "../types/target";
-  import {domainOption} from "../stores/domain-option";
-  import {redirectsTable, type CountStats, tableCountStats} from "../stores/target";
-=======
-  import RouteCreator from "../components/RouteCreator.svelte";
-  import RouteRow from "../components/RouteRow.svelte";
-  import {getBearer} from "../stores/login";
-  import type {CSPair} from "../types/cspair";
-  import {type Route, routeEqual} from "../types/target";
-  import {domainOption} from "../stores/domain-option";
-  import {routesTable, type CountStats, tableCountStats} from "../stores/target";
->>>>>>> ../views/RoutesView.svelte
+<script lang="ts" context="module">
+  interface Target {
+    src: string;
+  }
+</script>
 
-  const apiViolet = import.meta.env.VITE_API_VIOLET;
+<script lang="ts">
+  import {writable, type Writable} from "svelte/store";
+
+  import {getBearer} from "../stores/login";
+  import type {CSPair} from "../types/cspair";
+  import {domainOption} from "../stores/domain-option";
+  import {type CountStats, tableCountStats} from "../stores/target";
+
+  type T = $$Generic<Target>;
+
+  export let apiUrl: string;
+  export let tableData: Writable<{[key: string]: CSPair<T>}>;
+  export let equality: (a: T | null, b: T | null) => boolean;
 
   let tableKeys: string[] = [];
 
-<<<<<<< TargetManagement.svelte
-  $: tableKeys = Object.entries($redirectsTable)
-=======
-  $: tableKeys = Object.entries($routesTable)
->>>>>>> ../views/RoutesView.svelte
+  $: tableKeys = Object.entries($tableData)
     .filter(x => x[1].client != null || x[1].server != null)
     .map(x => x[0])
     .filter(x => domainFilter(x, $domainOption))
@@ -33,11 +28,7 @@
 
   let rowStats: CountStats = {created: 0, modified: 0, removed: 0};
 
-<<<<<<< TargetManagement.svelte
-  $: rowStats = tableCountStats($redirectsTable, tableKeys, redirectEqual);
-=======
-  $: rowStats = tableCountStats($routesTable, tableKeys, routeEqual);
->>>>>>> ../views/RoutesView.svelte
+  $: rowStats = tableCountStats($tableData, tableKeys, equality);
 
   function domainFilter(src: string, domain: string) {
     if (domain == "*") return true;
@@ -48,48 +39,28 @@
     return p.endsWith(domain);
   }
 
-<<<<<<< TargetManagement.svelte
-  let promiseForTable: Promise<void> = Object.keys($redirectsTable).length === 0 ? reloadTable() : Promise.resolve();
+  let promiseForTable: Promise<void> = Object.keys($tableData).length === 0 ? reloadTable() : Promise.resolve();
 
   function reloadTable(): Promise<void> {
     return new Promise<void>((res, rej) => {
-      fetch(apiViolet + "/redirect", {headers: {Authorization: getBearer()}})
-=======
-  let promiseForTable: Promise<void> = Object.keys($routesTable).length === 0 ? reloadTable() : Promise.resolve();
-
-  function reloadTable(): Promise<void> {
-    return new Promise<void>((res, rej) => {
-      fetch(apiViolet + "/route", {headers: {Authorization: getBearer()}})
->>>>>>> ../views/RoutesView.svelte
+      fetch(apiUrl, {headers: {Authorization: getBearer()}})
         .then(x => {
           if (x.status !== 200) throw new Error("Unexpected status code: " + x.status);
           return x.json();
         })
         .then(x => {
-<<<<<<< TargetManagement.svelte
-          let rows = x as Redirect[];
-          let srcs = new Set(Object.keys($redirectsTable));
+          let rows = x as T[];
+          let srcs = new Set(Object.keys($tableData));
           rows.forEach(x => {
-            $redirectsTable[x.src] = {
-              client: !$redirectsTable[x.src] ? JSON.parse(JSON.stringify(x)) : $redirectsTable[x.src]?.client,
-=======
-          let rows = x as Route[];
-          let srcs = new Set(Object.keys($routesTable));
-          rows.forEach(x => {
-            $routesTable[x.src] = {
-              client: !$routesTable[x.src] ? JSON.parse(JSON.stringify(x)) : $routesTable[x.src]?.client,
->>>>>>> ../views/RoutesView.svelte
+            $tableData[x.src] = {
+              client: !$tableData[x.src] ? JSON.parse(JSON.stringify(x)) : $tableData[x.src]?.client,
               server: x,
               p: Promise.resolve(),
             };
             srcs.delete(x.src);
           });
           srcs.forEach(x => {
-<<<<<<< TargetManagement.svelte
-            $redirectsTable[x].server = null;
-=======
-            $routesTable[x].server = null;
->>>>>>> ../views/RoutesView.svelte
+            $tableData[x].server = null;
           });
           res();
         })
@@ -104,33 +75,19 @@
 
   function saveChanges() {
     let tableProm = tableKeys
-<<<<<<< TargetManagement.svelte
-      .map(x => $redirectsTable[x])
+      .map(x => $tableData[x])
       .filter(x => x.client != null || x.server != null)
-      .filter(x => !redirectEqual(x.client, x.server))
-      .map((x: CSPair<Redirect>): Savable<Redirect> => {
-=======
-      .map(x => $routesTable[x])
-      .filter(x => x.client != null || x.server != null)
-      .filter(x => !routeEqual(x.client, x.server))
-      .map((x: CSPair<Route>): Savable<Route> => {
->>>>>>> ../views/RoutesView.svelte
+      .filter(x => !equality(x.client, x.server))
+      .map((x: CSPair<T>): Savable<T> => {
         if (x.client == null && x.server != null) return {type: "del", v: x};
         return {type: "ins", v: x};
       })
       .sort((a, _) => (a.type === "del" ? -1 : a.type === "ins" ? 1 : 0))
       .map(x => {
-<<<<<<< TargetManagement.svelte
-        x.v.p = fetch(apiViolet + "/redirect", {
+        x.v.p = fetch(apiUrl, {
           method: x.type == "del" ? "DELETE" : "POST",
           headers: {Authorization: getBearer()},
-          body: JSON.stringify(x.type == "del" ? {src: (x.v.server as Redirect).src} : x.v.client),
-=======
-        x.v.p = fetch(apiViolet + "/route", {
-          method: x.type == "del" ? "DELETE" : "POST",
-          headers: {Authorization: getBearer()},
-          body: JSON.stringify(x.type == "del" ? {src: (x.v.server as Route).src} : x.v.client),
->>>>>>> ../views/RoutesView.svelte
+          body: JSON.stringify(x.type == "del" ? {src: (x.v.server as T).src} : x.v.client),
         }).then(x => {
           if (x.status !== 200) throw new Error("Unexpected status code: " + x.status);
         });
@@ -143,15 +100,18 @@
         alert("Some rows failed to save changes");
       });
   }
+
+  export function make(e: CustomEvent<T>) {
+    const x = e.detail as unknown as T;
+    $tableData[x.src] = {client: x, server: $tableData[x.src]?.server, p: Promise.resolve()};
+    tableKeys.push(x.src);
+    tableKeys = tableKeys;
+  }
 </script>
 
 <div class="wrapper">
   <div style="padding:8px;background-color:#bb7900;">
-<<<<<<< TargetManagement.svelte
-    Warning: This is currently still under development, however it DOES update the real server redirects
-=======
-    Warning: This is currently still under development, however it DOES update the real server routes
->>>>>>> ../views/RoutesView.svelte
+    Warning: This is currently still under development, however it DOES send updates to the real server
   </div>
 
   <div class="scrolling-area">
@@ -163,46 +123,16 @@
       <table>
         <thead>
           <tr>
-            <th>Source</th>
-            <th>Destination</th>
-            <th>Flags</th>
-<<<<<<< TargetManagement.svelte
-            <th>Code</th>
-=======
->>>>>>> ../views/RoutesView.svelte
-            <th>Description</th>
-            <th>Active</th>
-            <th>Option</th>
+            <slot name="headers" />
           </tr>
-<<<<<<< TargetManagement.svelte
-          <RedirectCreator
-            on:make={e => {
-              const x = e.detail;
-              $redirectsTable[x.src] = {client: x, server: $redirectsTable[x.src]?.server, p: Promise.resolve()};
-=======
-          <RouteCreator
-            on:make={e => {
-              const x = e.detail;
-              $routesTable[x.src] = {client: x, server: $routesTable[x.src]?.server, p: Promise.resolve()};
->>>>>>> ../views/RoutesView.svelte
-              tableKeys.push(x.src);
-              tableKeys = tableKeys;
-            }}
-          />
+          <slot name="creator" {make} />
         </thead>
         <tbody>
           {#each tableKeys as src (src)}
-<<<<<<< TargetManagement.svelte
-            {#await $redirectsTable[src].p}
+            {#await $tableData[src].p}
               <tr><td colspan="5">Loading...</td></tr>
             {:then _}
-              <RedirectRow bind:value={$redirectsTable[src]} />
-=======
-            {#await $routesTable[src].p}
-              <tr><td colspan="5">Loading...</td></tr>
-            {:then _}
-              <RouteRow bind:value={$routesTable[src]} />
->>>>>>> ../views/RoutesView.svelte
+              <slot name="row" value={writable($tableData[src])} />
             {:catch err}
               <tr><td colspan="5">Error loading row for {src}: {err}</td></tr>
             {/await}
@@ -220,21 +150,13 @@
   <div class="footer">
     <button class="btn-green" on:click={() => saveChanges()}>Save Changes</button>
     {#if rowStats.created > 0}
-<<<<<<< TargetManagement.svelte
-      <div class="meta-info">{rowStats.created} new redirect{rowStats.created > 1 ? "s" : ""}</div>
-=======
-      <div class="meta-info">{rowStats.created} new route{rowStats.created > 1 ? "s" : ""}</div>
->>>>>>> ../views/RoutesView.svelte
+      <div class="meta-info">{rowStats.created} new row{rowStats.created > 1 ? "s" : ""}</div>
     {/if}
     {#if rowStats.modified > 0}
       <div class="meta-info">{rowStats.modified} unsaved change{rowStats.modified > 1 ? "s" : ""}</div>
     {/if}
     {#if rowStats.removed > 0}
-<<<<<<< TargetManagement.svelte
-      <div class="meta-info">{rowStats.removed} removed redirect{rowStats.removed > 1 ? "s" : ""}</div>
-=======
-      <div class="meta-info">{rowStats.removed} removed route{rowStats.removed > 1 ? "s" : ""}</div>
->>>>>>> ../views/RoutesView.svelte
+      <div class="meta-info">{rowStats.removed} removed row{rowStats.removed > 1 ? "s" : ""}</div>
     {/if}
   </div>
 </div>
