a8db4efca12c4e232b90b208200fa6288bb98a1f
MrMelon54 <github@mrmelon54.com>
2025-11-26 22:46:07 +0000

    Improve styling of dynamic list widget

--- src/components/DynamicListView.svelte
+++ src/components/DynamicListView.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import RemoveIcon from "../icons/Remove.svelte";
+
   type T = $$Generic<Object>;
 
   export let list: T[];
@@ -19,16 +21,54 @@
   }
 </script>
 
-<div>
+<div class="list-search">
   <input type="text" bind:value={addValue} />
   <button on:click={() => addItem()}>Add</button>
 </div>
 
-<div>
+<div class="list-view">
   {#each list as item, i}
-    <div>
-      {item}
-      <button on:click={() => removeItem(i)}>Remove</button>
+    <div class="list-item">
+      <div class="content">{item}</div>
+      <button class="remove" on:click={() => removeItem(i)}><RemoveIcon /></button>
     </div>
   {/each}
 </div>
+
+<style lang="scss">
+  @import "../values.scss";
+
+  .list-search {
+    display: flex;
+    flex-direction: row;
+    gap: 8px;
+
+    input {
+      flex-grow: 1;
+    }
+  }
+
+  .list-view {
+    display: flex;
+    flex-direction: column;
+
+    .list-item {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      .content {
+        padding: 8px;
+        font-family: monospace;
+      }
+
+      &:hover {
+        background-color: #262626;
+      }
+
+      .remove {
+        @include button-red-highlight;
+      }
+    }
+  }
+</style>
