6710211ac2a04e34ecd79dc928f1e1abe66e3fda
MrMelon54 <github@mrmelon54.com>
2025-11-27 01:22:14 +0000

    Remove Tom Select to use standard HTML datalist instead

--- src/components/CountrySelect.svelte
+++ src/components/CountrySelect.svelte
@@ -1,26 +1,15 @@
 <script lang="ts">
-  import TomSelect from "tom-select";
-  import {getCountryDataList, getEmojiFlag} from "countries-list";
-  import {onDestroy, onMount} from "svelte";
-
-  import "tom-select/dist/css/tom-select.css";
+  import {getCountryDataList} from "countries-list";
 
   const countriesList = getCountryDataList().sort((a, b) => a.name.localeCompare(b.name));
 
-  let countrySelectElement: HTMLSelectElement;
-  let countryTomSelect: TomSelect;
-
-  onMount(() => {
-    countryTomSelect = new TomSelect(countrySelectElement, {});
-  });
-
-  onDestroy(() => {
-    countryTomSelect.destroy();
-  });
+  export let value: string = "";
 </script>
 
-<select bind:this={countrySelectElement} {...$$restProps} autocomplete="false" style="display:none;">
-  {#each countriesList as x}
-    <option value={x.iso2}>{x.name}</option>
+<input bind:value list="countries-list" {...$$restProps} />
+
+<datalist id="countries-list">
+  {#each countriesList as country}
+    <option value={country.iso2}>{country.name}</option>
   {/each}
-</select>
+</datalist>
