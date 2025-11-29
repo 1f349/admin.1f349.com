bad73376d85f4dc42d45b8b35de93c253eac8f9f
MrMelon54 <github@mrmelon54.com>
2025-11-29 15:42:45 +0000

    Add button element around certificate auto renew icon

--- src/views/CertificatesView.svelte
+++ src/views/CertificatesView.svelte
@@ -278,17 +278,18 @@
         <tr slot="ok" let:value class:cert-error={value.data.renew_failed} class="empty-row">
           <td>{value.data.id}</td>
           <td>{value.data.name}</td>
-          <td
-            class="auto-renew-toggle"
-            class:auto-renew-enabled={value.data.auto_renew}
-            title={"Auto Renew " + (value.data.auto_renew ? "Enabled" : "Disabled")}
-            on:click={() => setAutoRenewState(value, !value.data.auto_renew)}
-          >
-            {#if value.data.auto_renew}
-              <RefreshCW />
-            {:else}
-              <RefreshCWOff />
-            {/if}
+          <td class="auto-renew-toggle">
+            <button
+              class:auto-renew-enabled={value.data.auto_renew}
+              title={"Auto Renew " + (value.data.auto_renew ? "Enabled" : "Disabled")}
+              on:click={() => setAutoRenewState(value, !value.data.auto_renew)}
+            >
+              {#if value.data.auto_renew}
+                <RefreshCW />
+              {:else}
+                <RefreshCWOff />
+              {/if}
+            </button>
           </td>
           <td>{value.data.active}</td>
           <td>{value.data.renewing}</td>
